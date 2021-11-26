875f1b70f9b6f0a45547147f6adb1596aae58190
Daniel Saavedra <danielink28@gmail.com>
2021-11-25 21:34:52 -0500

    Ordered code prducto model

--- src/main/java/com/tienda/tiendaapi/modelo/Producto.java
+++ src/main/java/com/tienda/tiendaapi/modelo/Producto.java
@@ -1,18 +1,10 @@
 package com.tienda.tiendaapi.modelo;
 
 import com.mongodb.lang.NonNull;
-import com.tienda.tiendaapi.dto.ProductoDTO;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/*
-Desde el navegador web o browser,
- el sistema mostrará una página que incluirá un formulario en donde el Administrador del sistema podrá
-  Agregar los datos de los productos de aseo a la BD, es decir,
-   registrar su referencia, marca, categoría, materiales, descripción, disponibilidad (SI/NO), precio, cantidad en stock y
-    fotografía (URL). En el momento de hacer clic en el botón Guardar el sistema creará la ficha de producto.
- */
 @Document(collection = "productos")
 @Data
 public class Producto {
@@ -23,6 +15,12 @@ public class Producto {
     private String referencia;
     @NonNull
     private String marca;
+    @NonNull
+    private Boolean disponibilidad;
+    @NonNull
+    private Integer precio;
+    @NonNull
+    private Integer cantidad;
 
     private String materiales;
 
@@ -30,11 +28,5 @@ public class Producto {
     private String categoria;
     private String descripcion;
 
-    @NonNull
-    private Boolean disponibilidad;
-    @NonNull
-    private Integer precio;
-    @NonNull
-    private Integer cantidad;
     private String urlFotografia;
 }
